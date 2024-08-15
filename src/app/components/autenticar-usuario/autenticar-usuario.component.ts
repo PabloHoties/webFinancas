4dc04d9dda8fb42c0ec7ff167f72730b610ec95d
PabloHoties <pablohotiesmarques@gmail.com>
2024-08-14 23:09:26 -0300

    desenvolvimento de "autenticar-usuario"

--- src/app/components/autenticar-usuario/autenticar-usuario.component.ts
+++ src/app/components/autenticar-usuario/autenticar-usuario.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-autenticar-usuario',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './autenticar-usuario.component.html',
   styleUrl: './autenticar-usuario.component.css'
 })
 export class AutenticarUsuarioComponent {
+
+  //criando a estrutura do formulário
+  form = new FormGroup({
+    /* campo 'email' */
+    email: new FormControl('', [Validators.required,
+    Validators.email]),
+    /* campo 'senha' */
+    senha: new FormControl('', [Validators.required,
+    Validators.minLength(8)])
+  });
+  //função para verificar os erros de validação de cada campo
+  get f() {
+    return this.form.controls;
+  }
+  //função para capturar o evento de submit do formulário
+  onSubmit() {
+    console.log(this.form.value);
+  }
 
 }
